52b7ca73e6d5be0fd777a04b7cae1b08fff29041
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-09-26 02:21:05 -0400

    Adds login and logout

--- server/app.py
+++ server/app.py
@@ -13,11 +13,46 @@ from config import app, db, api
 from models import User, Recipe, Ingredient, recipeIngredient
 
 
-# Views go here!
+class CheckSession(Resource):
+    pass
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+class SignUp(Resource):
+    pass
+class Login(Resource):
+    def post(self):
+        request_login=request.get_json()
+        username = request_login['username']
+        password = request_login['password']
+        user = User.query.filter(User.username == username).first()
+        if not session['user_id']:
+            return {'errors':'invalid username/password'}, 401
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        
+class Logout(Resource):
+    def delete(self):
+        if not session['user_id']:
+            return {'error': 'Unauthorized access'}, 401
+        if session.get('user_id'):
+            session['user_id'] = None
+            return {}, 204  
+    
+class RecipeAll(Resource):
+    pass
+class RecipeMember(Resource):
+    pass
+
+# @app.route('/')
+# def index():
+#     return '<h1>Project Server</h1>'
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(SignUp, '/signup')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(RecipeAll, '/recipe_all')
+api.add_resource(RecipeMember, '/recipe_member')
 
 
 if __name__ == '__main__':
