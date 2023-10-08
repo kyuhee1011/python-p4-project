09912cac562a83a06730f526f3488be549c1edec
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-10-07 23:30:10 -0400

    Updates My Favorite Page design

--- client/src/components/MyFav/MyFav.js
+++ client/src/components/MyFav/MyFav.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./MyFav.css";
 import Ingredient from "../Ingredient/Ingredient";
-import { Container, Row, Button } from "react-bootstrap";
+import { Container, Row, Button, Card } from "react-bootstrap";
 
 function MyFav({ user, recipes, handleUpdateFavorite }) {
   const recipeFilterArray = recipes.filter((recipe) => {
@@ -9,35 +9,52 @@ function MyFav({ user, recipes, handleUpdateFavorite }) {
   });
 
   return (
-    <Container>
-      <Row>
-        <h3 className="recipeTitle">My Favorite Recipes</h3>
-      </Row>
+    <Container className="containerCenter">
+      <Card className="contentCenter">
+        <Row>
+          <h3 className="recipeTitle">My Favorite Recipes</h3>
+        </Row>
+        <Card.Body className="contentBody">
+          <Row>
+            {recipeFilterArray.map((recipe) => (
+              <div key={recipe.id}>
+                <Card.Title className="recipeMainTitle">
+                  {recipe.title}
+                </Card.Title>
 
-      <Row>
-        {recipeFilterArray.map((recipe) => (
-          <div key={recipe.id}>
-            <h3 className="recipeMainTitle">{recipe.title}</h3>
+                <Card.Img
+                  className="imageSize"
+                  src={recipe.image_food}
+                  alt="My Delicious Food"
+                />
+                <Card.Text className="recipeDescript">
+                  {recipe.description}
+                </Card.Text>
+                <Card.Text className="recipeDescript">
+                  <b>Duration:</b> {recipe.duration}
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                  <b>Serving:</b> {recipe.serving}
+                </Card.Text>
+                <Card.Text className="recipeDescript">
+                  <b>Meal Type:</b>
+                  {recipe.mealType}
+                </Card.Text>
 
-            <img src={recipe.image_food} alt="My Delicious Food" />
-            <p className="recipeDescript">{recipe.description}</p>
-            <p>{recipe.duration}</p>
-            <p>{recipe.serving}</p>
-            <p>{recipe.mealType}</p>
+                <Ingredient recipe={recipe} />
 
-            <Ingredient recipe={recipe} />
-
-            <Button
-              type="submit"
-              variant={recipe.favorite ? "primary" : "outline-primary"}
-              onClick={() => handleUpdateFavorite(recipe.id)}
-              className="formEdit"
-            >
-              {recipe.favorite ? "unfavorite" : "favorite"}
-            </Button>
-          </div>
-        ))}
-      </Row>
+                <Button
+                  type="submit"
+                  variant={recipe.favorite ? "primary" : "outline-primary"}
+                  onClick={() => handleUpdateFavorite(recipe.id)}
+                  className="formEdit"
+                >
+                  {recipe.favorite ? "♡ Unfavorite" : "❤️ Favorite"}
+                </Button>
+              </div>
+            ))}
+          </Row>
+        </Card.Body>
+      </Card>
     </Container>
   );
 }
